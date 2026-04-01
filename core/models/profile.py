5384e2dd3770dba7b812535818bad04d78ff8408
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-01 12:25:41 -0300

    FIX: latitude, longitude validators

--- core/models/profile.py
+++ core/models/profile.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from . import ServiceType, User
@@ -5,14 +6,31 @@ from . import ServiceType, User
 
 class ProviderProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='provider_profile')
-    fixed_latitude = models.DecimalField(max_digits=9, decimal_places=6,)
-    fixed_longitude = models.DecimalField(max_digits=9, decimal_places=6,)
-    last_latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    last_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    fixed_latitude = models.DecimalField(
+        max_digits=9, decimal_places=6, validators=[MinValueValidator(-90), MaxValueValidator(90)]
+    )
+    fixed_longitude = models.DecimalField(
+        max_digits=9, decimal_places=6, validators=[MinValueValidator(-90), MaxValueValidator(90)]
+    )
+    last_latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)],
+        null=True,
+        blank=True,
+    )
+    last_longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)],
+        null=True,
+        blank=True,
+    )
     service_type = models.ForeignKey(ServiceType, on_delete=models.CASCADE, related_name='providers')
     price_per_hour = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     price_per_day = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -21,8 +39,20 @@ class ProviderProfile(models.Model):
 
 class ClientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client_profile')
-    last_latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    last_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    last_latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)],
+        null=True,
+        blank=True,
+    )
+    last_longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)],
+        null=True,
+        blank=True,
+    )
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
